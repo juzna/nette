d2e02034d2fe93f219dc297d46e94c4dd7ae40cb
Jan Dolecek <juzna.cz@gmail.com>
2012-10-06 15:17:44 +0200

    Tests: log database queries

--- tests/Nette/Database/connect.inc.php
+++ tests/Nette/Database/connect.inc.php
@@ -28,3 +28,32 @@ TestHelpers::lock($current['dsn'], dirname(TEMP_DIR));
 
 unset($config, $current, $rc);
 $driverName = $connection->getAttribute(PDO::ATTR_DRIVER_NAME);
+
+
+
+/**
+ * Logs executed queries
+ */
+class QueryLogger
+{
+	public static $queries;
+
+
+
+	public static function log(\Nette\Database\Statement $result, $params)
+	{
+		self::$queries[] = array(
+			$result->queryString,
+			$params
+		);
+	}
+
+
+	public static function clear()
+	{
+		self::$queries = array();
+	}
+
+}
+
+$connection->onQuery[] = callback('QueryLogger::log');
